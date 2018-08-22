c87a4a3d83ddc07f34e418e4017164e0a42cb6ca
Isaac Omondi <iomondi90@gmail.com>
2018-08-22 13:23:34 +0300

    created a newDeck() function using for loops iteration

--- deck.go
+++ deck.go
@@ -8,11 +8,26 @@ import (
 //which is a slice of stings
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"One", "Two", "Three", "Four"}
+
+	// we use the underscore if the first variable in the for loop is not being used anywhere
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
 // d: The actual copy of the deck we're working that is available in the function
 // In TypeScript it can be referenced as 'this' or in Python as 'self'
-// any variable of type 'deck' now gets access to the 'print' method 
+// any variable of type 'deck' now gets access to the 'print' method
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
