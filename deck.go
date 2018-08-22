248e8c74cc95618fb19f4aec1e65ffe73b606921
Isaac Omondi <iomondi90@gmail.com>
2018-08-22 16:49:41 +0300

    Write to local machine with newDeckFromFile function()

--- deck.go
+++ deck.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"os"
 	"io/ioutil"
 	"fmt"
 	"strings"
@@ -42,6 +43,15 @@ func (d deck) toString() string{
 	return strings.Join([]string(d), ",")
 }
 
+// []byte() turns string into bytes
 func (d deck) saveToFile(filename string) error {
 	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ",err)
+		os.Exit(1)
+	}
 }
